83c75dd8c25932d49385b8d59f422f9ec9ec817e
Lucas Rudi <rudilucas@gmail.com>
2014-07-19 17:13:44 -0300

    Iniciando este experimento magico

---
+++ kit_de_medicion.ino
@@ -0,0 +1,135 @@
+/*--------------------------------------------------------------
+  Program:      eth_websrv_AJAX_switch
+
+  Description:  Arduino web server shows the state of a switch
+                on a web page using AJAX. The state of the
+                switch must be read by clicking a button on
+                the web page - for demonstrating AJAX.
+                Does not use the SD card.
+  
+  Hardware:     Arduino Uno and official Arduino Ethernet
+                shield. Should work with other Arduinos and
+                compatible Ethernet shields.
+                
+  Software:     Developed using Arduino 1.0.3 software
+                Should be compatible with Arduino 1.0 +
+  
+  References:   - WebServer example by David A. Mellis and 
+                  modified by Tom Igoe
+                - Ethernet library documentation:
+                  http://arduino.cc/en/Reference/Ethernet
+                - Learning PHP, MySQL & JavaScript by
+                  Robin Nixon, O'Reilly publishers
+
+  Date:         15 January 2013
+ 
+  Author:       W.A. Smith, http://startingelectronics.com
+--------------------------------------------------------------*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+// MAC address from Ethernet shield sticker under board
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(10, 0, 0, 20); // IP address, may need to change depending on network
+EthernetServer server(80);  // create a server at port 80
+
+String HTTP_req;            // stores the HTTP request
+
+void setup()
+{
+    Ethernet.begin(mac, ip);  // initialize Ethernet device
+    server.begin();           // start to listen for clients
+    Serial.begin(9600);       // for diagnostics
+    pinMode(3, INPUT);        // switch is attached to Arduino pin 3
+}
+
+void loop()
+{
+    EthernetClient client = server.available();  // try to get client
+
+    if (client) {  // got client?
+        boolean currentLineIsBlank = true;
+        while (client.connected()) {
+            if (client.available()) {   // client data available to read
+                char c = client.read(); // read 1 byte (character) from client
+                HTTP_req += c;  // save the HTTP request 1 char at a time
+                // last line of client request is blank and ends with \n
+                // respond to client only after last line received
+                if (c == '\n' && currentLineIsBlank) {
+                    // send a standard http response header
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-Type: text/html");
+                    client.println("Connection: keep-alive");
+                    client.println();
+                    // AJAX request for switch state
+                    if (HTTP_req.indexOf("ajax_switch") > -1) {
+                        // read switch state and send appropriate paragraph text
+                        GetSwitchState(client);
+                    }
+                    else {  // HTTP request for web page
+                        // send web page - contains JavaScript with AJAX calls
+                        client.println("<!DOCTYPE html>");
+                        client.println("<html>");
+                        client.println("<head>");
+                        client.println("<title>Arduino Web Page</title>");
+                        client.println("<script>");
+                        client.println("function GetSwitchState() {");
+                        client.println("nocache = \"&nocache=\"\
+                                                         + Math.random() * 1000000;");
+                        client.println("var request = new XMLHttpRequest();");
+                        client.println("request.onreadystatechange = function() {");
+                        client.println("if (this.readyState == 4) {");
+                        client.println("if (this.status == 200) {");
+                        client.println("if (this.responseText != null) {");
+                        client.println("document.getElementById(\"switch_txt\")\
+.innerHTML = this.responseText;");
+                        client.println("}}}}");
+                        client.println(
+                        "request.open(\"GET\", \"ajax_switch\" + nocache, true);");
+                      //client.println("request.open(\"GET\", \"ajax_switch\", true);");
+                        client.println("request.send(null);");
+                        client.println("}");
+                        client.println("</script>");
+                        client.println("</head>");
+                        client.println("<body>");
+                        client.println("<h1>Arduino AJAX Switch Status</h1>");
+                        client.println(
+                        "<p id=\"switch_txt\">Switch state: Not requested...</p>");
+                        client.println("<button type=\"button\"\
+                            onclick=\"GetSwitchState()\">Get Switch State</button>");
+                        client.println("</body>");
+                        client.println("</html>");
+                    }
+                    // display received HTTP request on serial port
+                    Serial.print(HTTP_req);
+                    HTTP_req = "";            // finished with request, empty string
+                    break;
+                }
+                // every line of text received from the client ends with \r\n
+                if (c == '\n') {
+                    // last character on line of received text
+                    // starting new line with next character read
+                    currentLineIsBlank = true;
+                } 
+                else if (c != '\r') {
+                    // a text character was received from client
+                    currentLineIsBlank = false;
+                }
+            } // end if (client.available())
+        } // end while (client.connected())
+        delay(1);      // give the web browser time to receive the data
+        client.stop(); // close the connection
+    } // end if (client)
+}
+
+// send the state of the switch to the web browser
+void GetSwitchState(EthernetClient cl)
+{
+    if (digitalRead(3)) {
+        cl.println("Switch state: ON");
+    }
+    else {
+        cl.println("Switch state: OFF");
+    }
+}
