683ae068427c74fd1cc77649cdf1ad01d8fc745e
Davo <davidgalavotti@gmail.com>
2014-07-20 13:22:01 -0300

    Agrego version de la visualizacion aun rota

--- webserver/public/js/viz.js
+++ webserver/public/js/viz.js
@@ -1,70 +1,105 @@
-'use strict';
+(function() {
 
-var n = 10,
-    random = d3.random.normal(0, .1),
-    data = d3.range(n).map(random);
+  'use strict';
 
-var margin = {top: 0, right: 0, bottom: 0, left: 0},
-    width = 1280 - margin.left - margin.right,
-    height = 720 - margin.top - margin.bottom;
+  var data = [],
+      timeout;
 
-var x = d3.scale.linear()
-    .domain([1, n - 2])
-    .range([0, width]);
+  function loadData() {
+    d3.json("/api/v1/", function(error, datajson) {
+      timeout = setTimeout(loadData, 1000);
+      console.log(datajson.Temperatura);
+    });
+  }
 
-var y = d3.scale.linear()
-    .domain([-1, 1])
-    .range([height, 0]);
+  var n = 10,
+      random = d3.random.normal(0, .4);
 
-var line = d3.svg.line()
-    .interpolate("basis")
-    .x(function(d, i) { return x(i); })
-    .y(function(d, i) { return y(d); });
+      data = d3.range(n).map(random);
 
-var svg = d3.select("#temperatura").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .attr("shape-rendering","optimizeSpeed")
-      .attr("color-rendering","optimizeSpeed")
-      .attr("viewBox", "0 0 1280 720")
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var margin = {top: 0, right: 0, bottom: 0, left: 0},
+      width = 1280 - margin.left - margin.right,
+      height = 720 - margin.top - margin.bottom;
 
-svg.append("defs").append("clipPath")
-    .attr("id", "clip")
-  .append("rect")
-    .attr("width", width)
-    .attr("height", height);
+  var x = d3.scale.linear()
+      .domain([1, n - 2])
+      .range([0, width]);
 
-svg.append("g")
-    .attr("class", "y axis")
-    .call(d3.svg.axis().scale(y).orient("right"));
+  var y = d3.scale.linear()
+      .domain([-1, 1])
+      .range([height, 0]);
 
-var path = svg.append("g")
-    .attr("clip-path", "url(#clip)")
-  .append("path")
-    .datum(data)
-    .attr("class", "line")
-    .attr("d", line);
+  var line = d3.svg.line()
+      .interpolate("basis")
+      .x(function(d, i) { return x(i); })
+      .y(function(d, i) { return y(d); });
 
-tick();
+  // var x = d3.scale.linear()
+  //     .domain([1, n - 2])
+  //     .range([0, width]);
 
-function tick() {
+  // var y = d3.scale.linear()
+  //     .domain([0, d3.max(data, function(d) { return d[1]; })])
+  //     .range([height, 0]);
 
-  // push a new data point onto the back
-  data.push(random());
+  // var line = d3.svg.line()
+  //     .interpolate("basis")
+  //     .x(function(d, i) { return x(i); })
+  //     .y(function(d, i) { return y(d); });
 
-  // redraw the line, and slide it to the left
-  path
-      .attr("d", line)
-      .attr("transform", null)
-    .transition()
-      .duration(500)
-      .ease("linear")
-      .attr("transform", "translate(" + x(0) + ",0)")
-      .each("end", tick);
 
-  // pop the old data point off the front
-  data.shift();
+  // var temperatura = d3.select("#temperatura .numero .valor")
+  //     .datum(data)
+  //     .html(function d(){d.Temperatura});
 
-}
+  var svg = d3.select("#temperatura").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("shape-rendering","optimizeSpeed")
+        .attr("color-rendering","optimizeSpeed")
+        .attr("viewBox", "0 0 1280 720")
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.append("defs").append("clipPath")
+      .attr("id", "clip")
+    .append("rect")
+      .attr("width", width)
+      .attr("height", height);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(d3.svg.axis().scale(y).orient("right"));
+
+  var path = svg.append("g")
+      .attr("clip-path", "url(#clip)")
+    .append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", line);
+
+  tick();
+
+  function tick() {
+
+    // push a new data point onto the back
+    data.push(random());
+
+    loadData();
+
+    // redraw the line, and slide it to the left
+    path
+        .attr("d", line)
+        .attr("transform", null)
+      .transition()
+        .duration(500)
+        .ease("linear")
+        .attr("transform", "translate(" + x(0) + ",0)")
+        .each("end", tick);
+
+    // pop the old data point off the front
+    data.shift();
+
+  }
+
+})()
